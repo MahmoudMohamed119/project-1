848512c198346a144a4b9db35820ae56f83f496f
MahmoudMohamed119 <137166856+MahmoudMohamed119@users.noreply.github.com>
2023-07-01 17:20:56 +0300

    Add files via upload
    code java

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+      //  int i=9 , j=2;
+      /*  System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++j);
+     */
+
+      /*  i+=5;
+        j*=20;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(++i*--j *2);
+     */
+
+    /*  int number;
+        System.out.println("Enter number to test");
+        number= input.nextInt();
+        if (number%2==0){
+            System.out.println("Number is even");
+        }
+        else
+            System.out.println("number is odd");
+       */
+
+     /*   int n;
+        for (int i=0;i<5;i++) {
+            System.out.println("Enter your degree");
+            n = input.nextInt();
+            if (n <= 0 || n >= 100) {
+                System.out.println("Anvaild Grade");
+            } else if (n > 90)
+                System.out.println("you got A");
+            else if (n > 80)
+                System.out.println("you got B");
+            else if (n > 60)
+                System.out.println("you got c");
+            else if (n > 50)
+                System.out.println("you got D");
+            else if (n < 50)
+                System.out.println("you got F");
+        }
+      */
+
+      /* char grade ; int counter=1;
+       while (counter<=5){
+        grade= input.next().charAt(0);
+         switch (grade) {
+             case 'A':
+                 System.out.println("Excellent");
+                 break;
+             case 'B':
+                 System.out.println("Very good");
+                 break;
+             case 'C':
+                 System.out.println("Good");
+                 break;
+             case 'D':
+                 System.out.println("Fair");
+                 break;
+             case 'F':
+                 System.out.println("Failed");
+                 break;
+             default:
+                 System.out.println("Anvaild Grade");
+         }
+         counter++;
+        }
+       System.out.println("you finished");
+       */
+
+
+    }
+}
